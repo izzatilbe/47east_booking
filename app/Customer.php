6cc122a30f8b859b22d63b21baa4a52313e83b0e
izzatilbe <izzatilbe@gmail.com>
2020-02-11 10:17:58 +0800

    revert

--- app/Customer.php
+++ app/Customer.php
@@ -2,9 +2,55 @@
 
 namespace App;
 
+use App\Traits\Auditable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    //
+    /*
+    use SoftDeletes, Auditable;
+
+    public $table = 'customers';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'email',
+        'phone',
+        'skype',
+        'address',
+        'website',
+        'last_name',
+        'first_name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'description',
+    ];
+
+    public function bookedByDormitoryBookings()
+    {
+        return $this->hasMany(DormitoryBooking::class, 'booked_by_id', 'id');
+    }
+
+    public function bookedByVenueBookings()
+    {
+        return $this->hasMany(VenueBooking::class, 'booked_by_id', 'id');
+    }
+
+    public function bookedByStaycationBookings()
+    {
+        return $this->hasMany(StaycationBooking::class, 'booked_by_id', 'id');
+    }
+
+    public function bookedByCoworkings()
+    {
+        return $this->hasMany(Coworking::class, 'booked_by_id', 'id');
+    }
+    */
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-Route::get('/', 'CustomerPagesController@index');
-//Route::redirect('/', '/login');
+Route::redirect('/', '/login');
 Route::get('/home', function () {
     if (session('status')) {
         return redirect()->route('admin.home')->with('status', session('status'));
